376a76f38e7c49c79938b4f9981a9a30738d837b
iCruzE7 <ice7xd@hotmail.com>
2018-02-12 09:27:17 -0700

    Update __init__.py

--- caso/__init__.py
+++ caso/__init__.py
@@ -6,17 +6,17 @@ import caso.listado as listado
 import caso.datos as datos
 
 def principal():
-    while True:
+    while True:	#ciclo infinito
         try:
             alumnos = input("Número de alumnos:")
             alumnos = int(alumnos)
             print()
         except (ValueError) as error:
             print(error)
-            continue
-        else:
-            break
-    for contador in range(alumnos):
+            continue	# Vuelve a ejecutar el ciclo y pide el número de alumnos
+        else:	# no se genera la excepción
+            break	# Rompe el while y entra al for
+    for contador in range(alumnos):	# 0 < alumnos
         print("\nAlumno nuevo", contador + 1)
         datos.alumnos.append(altas.alta())
     listado.despliega_todos()
