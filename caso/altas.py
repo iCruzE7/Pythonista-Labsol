5c679e7db47d293f06cbc09ce98806da4049d915
iCruzE7 <ice7xd@hotmail.com>
2018-02-12 09:28:20 -0700

    Update altas.py

--- caso/altas.py
+++ caso/altas.py
@@ -3,26 +3,26 @@
 import caso.datos as datos
 import caso.valida as valida
 """Módulo que contiene la funcion de altas."""
-""""""
-
 
 def alta():
     """Realiza las altas."""
-    
-    
+        
     def ingresa(campo):
+        """Recibe como argumento el identificador de datos.orden"""
         while True:
             mensaje = "Ingrese " + campo.lower() + ": " 
             dato = input(mensaje)
-            if datos.campos[campo] != str:
+            if datos.campos[campo] != str:  #Si no se está solicitando un String
                 try:
-                    if eval(dato) == datos.campos[campo](dato):
+                    #Si el dato ingresado es del tipo esperado
+                    if eval(dato) == datos.campos[campo](dato): 
                         dato = datos.campos[campo](dato)
                     else:
-                        continue
+                        continue    #Vuelve al while a solicitar el dato
                 except:
-                    continue
-            if valida.reglas(dato, campo):
+                    continue    #Vuelve al while a solicitar el dato
+            if valida.reglas(dato, campo):  #si la info ingresada es válida
                 return dato
-                  
-    return {campo: ingresa(campo) for campo in datos.orden}
+	#(Completado de Elementos) Solicita los 7 campos (llama la función "ingresa") 
+	#uno por vez que debe contener cada alumno y los Retorna para que se haga el #append                  
+    return {campo: ingresa(campo) for campo in datos.orden} 
